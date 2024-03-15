6129aa36a2601e4e6dbc2bdb3e1d722fabf3ff27
Dmitrii Tourkin <ddiimmuuzz@yandex.ru>
2024-03-16 01:39:24 +0300

    Доделать и пересмотреть

--- src/App.js
+++ src/App.js
@@ -6,23 +6,44 @@ function App() {
   const [fact, setFact] = useState({});
   const textareaRef = useRef(null);
 
+  const [name, setName] = useState(null);
+  const guessNameRef = useRef('');
+  let timerId;
+
+
   const handleTaskChangeButton = () => {
     setTaskNumber(prevTaskNumber => prevTaskNumber === 0 ? 1 : 0);
   }
   
   const handleButtonClick = async () => {    
     let url = 'https://catfact.ninja/fact';
-    
     const result = await fetch(url).then(data => data.json());
     setFact(result);
   }
 
-  const handleFormButton = () => {
-    console.log('Нажато');
+  const handleFormButton = async (e) => {
+    e.preventDefault();
+    await guessAgeByName();
   }
 
-  const handleInputChange = () => {
-    console.log('hi');
+  const handleInputChange = async (e) => {
+
+    e.preventDefault();
+    const name = e.target.value;
+    setName(name);
+    await guessAgeByName();
+  }
+
+  const guessAgeByName = async () => {
+    const url = 'https://api.agify.io/?name=' + name;
+
+    try {
+      const result = await fetch(url).then(data => data.json());
+      const age = result.age;
+      guessNameRef.current.textContent = age.toString();
+    } catch {
+      guessNameRef.current.textContent = 'Не получилось найти'
+    }
   }
 
   useEffect(() => {
@@ -47,8 +68,9 @@ function App() {
         <h1>Привет, а вот и форма!</h1>
         <form class='form'>
           <label>Имя</label>
-          <input className='form-input' placeholder='Введите имя' onChange={handleInputChange}></input>
-          <button type='submit' className='button' onClick={handleFormButton}></button>
+          <input className='form-input' name='name' id='name' value={name} placeholder='Введите имя' onChange={(e) => handleInputChange(e)}></input>
+          <p ref={guessNameRef} className='guessed-name'></p>
+          <button type='submit' className='button' onClick={(e) => handleFormButton(e)}></button>
         </form>
       </div>
     )}
