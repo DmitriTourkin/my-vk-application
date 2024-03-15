817d8c269c47eca6307cb99e73f114bb29233e23
Dmitrii Tourkin <ddiimmuuzz@yandex.ru>
2024-03-15 22:53:50 +0300

    Started VK UI, cleaned up manifest.json,

---
+++ src/AppVK.js
@@ -0,0 +1,38 @@
+import * as React from 'react';
+import { usePlatform } from '@vkontakte/vkui'
+import {
+  AppRoot,
+  SplitLayout,
+  SplitCol,
+  View,
+  Panel,
+  PanelHeader,
+  Header,
+  Group,
+  SimpleCell,
+} from '@vkontakte/vkui';
+import '@vkontakte/vkui/dist/vkui.css';
+
+const AppVK = () => {
+  const platform = usePlatform();
+
+  return (
+    <AppRoot>
+      <SplitLayout header={platform !== 'vkcom' && <PanelHeader delimiter="none" />}>
+        <SplitCol autoSpaced>
+          <View activePanel="main">
+            <Panel id="main">
+              <PanelHeader>VKUI</PanelHeader>
+              <Group header={<Header mode="secondary">Items</Header>}>
+                <SimpleCell>Hello</SimpleCell>
+                <SimpleCell>World</SimpleCell>
+              </Group>
+            </Panel>
+          </View>
+        </SplitCol>
+      </SplitLayout>
+    </AppRoot>
+  );
+};
+
+export default AppVK;
