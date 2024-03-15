4b74d846c154d648b2e9494af2e35e207eeedf23
Dmitrii Tourkin <ddiimmuuzz@yandex.ru>
2024-03-16 01:00:14 +0300

    2nd Task has been started

--- src/App.js
+++ src/App.js
@@ -2,14 +2,27 @@ import { useState, useEffect, useRef, createFactory } from 'react';
 import './App.css';
 
 function App() {
+  const [taskNumber, setTaskNumber] = useState(0);
   const [fact, setFact] = useState({});
   const textareaRef = useRef(null);
 
+  const handleTaskChangeButton = () => {
+    setTaskNumber(prevTaskNumber => prevTaskNumber === 0 ? 1 : 0);
+  }
+  
   const handleButtonClick = async () => {    
     let url = 'https://catfact.ninja/fact';
     
     const result = await fetch(url).then(data => data.json());
     setFact(result);
+  }
+
+  const handleFormButton = () => {
+    console.log('Нажато');
+  }
+
+  const handleInputChange = () => {
+    console.log('hi');
   }
 
   useEffect(() => {
@@ -21,13 +34,27 @@ function App() {
   }, [fact]);
 
   return (
+    <>
+    <button className='button' onClick={handleTaskChangeButton}>Сменить задание</button>
+    {!taskNumber ? (
     <div>
       <h1>Нажми на кнопку, взберись на сопку!</h1>
       <textarea ref={textareaRef} className='fact-text' value={fact.fact} ></textarea><br/>
-      <button className='button' onClick={() => handleButtonClick()}>Получить факт</button>
+      <button className='button' onClick={handleButtonClick}>Получить факт</button>
     </div>
+    ) : (
+      <div>
+        <h1>Привет, а вот и форма!</h1>
+        <form class='form'>
+          <label>Имя</label>
+          <input className='form-input' placeholder='Введите имя' onChange={handleInputChange}></input>
+          <button type='submit' className='button' onClick={handleFormButton}></button>
+        </form>
+      </div>
+    )}
+    </>
   );
-}
+};
 
 export default App;
 
